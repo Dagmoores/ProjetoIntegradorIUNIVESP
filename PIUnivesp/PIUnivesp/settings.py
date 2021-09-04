929b57cd74bddb8c55ba5d4ebc5d8c4ee8d3ed24
Daniel Moore <dagmoores@gmail.com>
2021-09-04 04:16:51 -0300

    correcao secret key anteriormente exposta

--- PIUnivesp/PIUnivesp/settings.py
+++ PIUnivesp/PIUnivesp/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-80f_6gps2$(6kcw2!_te7rk-1&gn3dvd4owx_y-*)nsn2)s@l+'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
