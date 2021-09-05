0e407feb54836809f718dd354513553077af0bbc
Daniel Moore <dagmoores@gmail.com>
2021-09-04 21:21:54 -0300

    Correcao da localizacao do arquivo dbimages

--- PIUnivesp/biblioteca/models.py
+++ PIUnivesp/biblioteca/models.py
@@ -7,7 +7,7 @@ class Books(models.Model):
     slug = models.SlugField(max_length=255, unique = True)
     author = models.CharField(max_length=100)
     body = models.TextField()
-    bookPhoto = models.ImageField(upload_to='./dbimages/', null = True, blank = True)
+    bookPhoto = models.ImageField(upload_to='./biblioteca/dbimages/', null = True, blank = True)
 
     def __str__(self):
         return self.title
