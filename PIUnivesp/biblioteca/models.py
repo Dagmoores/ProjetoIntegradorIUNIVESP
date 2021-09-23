13d517c73ad404d05ef0b1dac962b10ed0985b01
Daniel Moore <dagmoores@gmail.com>
2021-09-23 07:11:34 -0300

    Add Hyperlinks para os ebooks, add campo link no banco  de dados Adicionado o campo de hyperlink no banco de dados e relacionado o href ao link do banco de dados no arquivo books_detail. O link está relacionado com a foto.

--- PIUnivesp/biblioteca/models.py
+++ PIUnivesp/biblioteca/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.fields import NullBooleanField
+
 
 # Create your models here.
 
@@ -8,6 +10,7 @@ class Books(models.Model):
     author = models.CharField(max_length=100)
     body = models.TextField()
     bookPhoto = models.ImageField(upload_to = "books_Img/", null = True, blank = True)
+    link = models.CharField(max_length=255, default= NullBooleanField)
 
     def __str__(self):
         return self.title
