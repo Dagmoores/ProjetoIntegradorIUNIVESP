7a3090f1d8d1c0742ed0f3b6576d04236480155c
Dagmoores <dagmoores@gmail.com>
2021-10-21 23:00:20 -0300

    correcao do static_root e staticfiles_dirs

--- PIUnivesp/settings.py
+++ PIUnivesp/settings.py
@@ -120,8 +120,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), ) 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+  # Put strings here, like "/home/html/static" or "C:/www/django/static".
+  # Always use forward slashes, even on Windows.
+  # Don't forget to use absolute paths, not relative paths.
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
