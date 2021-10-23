7e6f8aca0ff73ac43a59fd97484e003d3402e4aa
Dagmoores <dagmoores@gmail.com>
2021-10-23 00:03:32 -0300

    mudancas no settings para conexao com postgresql

--- PIUnivesp/settings.py
+++ PIUnivesp/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'PIUnivesp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dkofamed',
+        'USER':'dkofamed',
+        'PASSWORD':'t_PrImXQs2uMgEwVwUix3QNBeKojICKT',
+        'HOST':'fanny.db.elephantsql.com ',
+        'PORT':'5432',
     }
 }
 
